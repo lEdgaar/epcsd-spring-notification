7fd49a76f553a87538d31533cffc9bf7c9a9cfc1
Edgar Doutón <edouton@uoc.edu>
2022-05-14 01:17:26 +0200

    Create a kafka listener
    Create a kafka listener

--- src/main/java/edu/uoc/epcsd/notification/controllers/NotificationController.java
+++ src/main/java/edu/uoc/epcsd/notification/controllers/NotificationController.java
@@ -1,6 +1,9 @@
 package edu.uoc.epcsd.notification.controllers;
 
+import edu.uoc.epcsd.notification.pojos.Show;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/notifications")
 public class NotificationController {
 
+    @KafkaListener(topics = "show-created", autoStartup = "${kafka-enabled}")
+    public void notification(Show show) {
+
+    }
 
 }
